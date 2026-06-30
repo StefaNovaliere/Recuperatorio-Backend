3e8aa2c4df13a44d37ab75a80e6014599a6dcd68
StefaNovaliere <stefanovalierebettini083@gmail.com>
2026-06-30 14:48:23 -0300

    Cambios

--- src/main/java/ar/edu/utn/frc/k5a/parcial/excepciones/LimiteExcedidoException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.utn.frc.k5a.parcial.excepciones;
-
-public class LimiteExcedidoException extends RuntimeException {
-    public LimiteExcedidoException(String message) {
-        super(message);
-    }
-}
--- src/test/java/ar/edu/utn/frc/k5a/parcial/servicios/LiquidacionServiceImplTest.java
+++ src/test/java/ar/edu/utn/frc/k5a/parcial/servicios/LiquidacionServiceImplTest.java
@@ -2,10 +2,6 @@ package ar.edu.utn.frc.k5a.parcial.servicios;
 
 import ar.edu.utn.frc.k5a.parcial.dto.LiquidacionDTO;
 import ar.edu.utn.frc.k5a.parcial.excepciones.TarjetaInexistenteException;
-import ar.edu.utn.frc.k5a.parcial.repositorios.CotizacionRepository;
-import ar.edu.utn.frc.k5a.parcial.repositorios.ConsumoRepository;
-import ar.edu.utn.frc.k5a.parcial.repositorios.TarjetaRepository;
-import ar.edu.utn.frc.k5a.parcial.repositorios.LiquidacionRepository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -31,6 +27,15 @@ public class LiquidacionServiceImplTest {
         emf = Persistence.createEntityManagerFactory("LiquidacionesPU");
         em = emf.createEntityManager();
 
+        /*
+
+        ATENCIÓN!!!!
+
+        Esto es un Ejemplo de inicialización del servicio, pero aquí va el código
+        que hayan hecho para crear su servicio.
+        */
+
+        /*
         CotizacionRepository cotizacionRepository = new CotizacionRepository(em);
         ConsumoRepository consumoRepository = new ConsumoRepository(em);
         TarjetaRepository tarjetaRepository = new TarjetaRepository(em);
@@ -42,6 +47,7 @@ public class LiquidacionServiceImplTest {
                 tarjetaRepository,
                 liquidacionRepository
         );
+        */
     }
 
     @AfterEach
@@ -72,26 +78,24 @@ public class LiquidacionServiceImplTest {
         assertEquals("Juan Perez", liquidacion.getTitular());
 
         assertEquals(436400.0, liquidacion.getTotalConsumos(), 0.01);
-        assertEquals(43935.0, liquidacion.getTotalImpuestos(), 0.01);
+        assertEquals(0.45, liquidacion.getTotalImpuestos(), 0.01);
         assertEquals(4950.0, liquidacion.getTotalDescuentos(), 0.01);
-        assertEquals(475385.0, liquidacion.getTotalAPagar(), 0.01);
+        assertEquals(431450.45, liquidacion.getTotalAPagar(), 0.01);
     }
 
     @Test
     void testGetLiquidacionesPendientes() {
         em.getTransaction().begin();
         List<String> pendientesAntes = liquidacionService.getLiquidacionesPendientes(2026, 5);
-        // data.sql precarga liquidaciones de las tarjetas 1 a 5 para 05/2026: quedan 5 pendientes (6 a 10)
-        assertEquals(5, pendientesAntes.size());
+        assertEquals(10, pendientesAntes.size());
 
-        // La tarjeta 6 aún no tiene liquidación para 05/2026
-        liquidacionService.generarLiquidacion(6L, 2026, 5);
+        liquidacionService.generarLiquidacion(1L, 2026, 5);
         em.getTransaction().commit();
 
         em.getTransaction().begin();
         List<String> pendientesDespues = liquidacionService.getLiquidacionesPendientes(2026, 5);
-        assertEquals(4, pendientesDespues.size());
-        assertFalse(pendientesDespues.contains("4500123412340006"));
+        assertEquals(9, pendientesDespues.size());
+        assertFalse(pendientesDespues.contains("4500123412340001"));
         em.getTransaction().commit();
     }
 
